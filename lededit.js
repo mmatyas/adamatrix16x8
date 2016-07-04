f66ae89fe5ec3d07c1c84f29a8300cd1c3209ca1
Mátyás Mustoha <mmatyas@inf.u-szeged.hu>
2016-07-04 12:30:26 +0200

    Refactor

--- lededit.js
+++ lededit.js
@@ -1,52 +1,143 @@
-var matrix;
-var canvas
+var board;
 var rotated = false;
-var cell_dim_px = 40;
 var bt_handle;
 var log_box;
+
+
+function Board(canvas_dom_elem, bt_callback) {
+	this.canvas = canvas_dom_elem;
+	this.bt_callback = bt_callback;
+	this.cell_dim_px = 40;
+
+	this.reset_mx();
+
+	this.ctx = this.canvas.getContext("2d");
+
+	var self = this;
+	this.canvas.addEventListener('click', function(event){
+		self.on_click(event);
+	}, false);
+
+	this.redraw();
+}
+
+Board.prototype.on_click = function(event) {
+	var x;
+	var y;
+	if (event.x != undefined && event.y != undefined) {
+		x = event.x;
+		y = event.y;
+	}
+	else { // Firefox method to get the position
+		x = event.clientX
+			+ (document.body.scrollLeft || 0)
+			+ (document.documentElement.scrollLeft || 0);
+		y = event.clientY
+			+ (document.body.scrollTop || 0)
+			+ (document.documentElement.scrollTop || 0);
+	}
+	x -= this.canvas.offsetLeft || 0;
+	y -= this.canvas.offsetTop || 0;
+	var row = Math.floor(y / this.cell_dim_px);
+	var col = Math.floor(x / this.cell_dim_px);
+	if (rotated) {
+		var tmp = col;
+		col = row;
+		row = 7 - tmp;
+	}
+
+	this.matrix[row][col] = !this.matrix[row][col];
+	this.redraw();
+	this.bt_callback()
+}
+
+Board.prototype.reset_mx = function() {
+	this.matrix = new Array(8);
+	for (var row = 0; row < 8; row++) {
+		this.matrix[row] = new Array(16);
+		for (var col = 0; col < 16; col++)
+			this.matrix[row][col] = false;
+	}
+}
+
+Board.prototype.redraw = function() {
+	this.ctx.shadowColor = '#f00';
+	this.ctx.fillStyle = "#000000";
+	this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+	var cell_offset = 3;
+	var cell_radius = (this.cell_dim_px - cell_offset * 2) / 2;
+	for (var row = 0; row < 8; row++) {
+		for (var col = 0; col < 16; col++) {
+			if (this.matrix[row][col]) {
+				this.ctx.fillStyle = "#ff0000";
+				this.ctx.shadowBlur = 20;
+			}
+			else {
+				this.ctx.fillStyle = "#ffefd5";
+				this.ctx.shadowBlur = 0;
+			}
+
+			this.ctx.beginPath();
+			this.ctx.arc(
+				cell_offset + col * this.cell_dim_px + cell_radius,
+				cell_offset + row * this.cell_dim_px + cell_radius,
+				cell_radius,
+				0, 2 * Math.PI);
+			this.ctx.fill();
+		}
+	}
+}
+
+Board.prototype.reset = function() {
+	this.reset_mx();
+	this.redraw();
+	this.bt_callback();
+
+}
+
+Board.prototype.fill_matrix = function(bt_arr) {
+	this.reset_mx();
+
+	var idx = 0;
+
+	for (var row = 0; row < 8; row++) {
+		var val = bt_arr[idx].charCodeAt(0);
+		for (var col = 0; col < 8; col++)
+			this.matrix[row][col] = ((val & 1 << col) > 0);
+
+		idx++;
+
+		val = bt_arr[idx].charCodeAt(0);
+		for (var col = 0; col < 8; col++)
+			this.matrix[row][col + 8] = ((val & 1 << col) > 0);
+
+		idx++;
+	}
+
+	this.redraw();
+}
+
+Board.prototype.flip = function() {
+	for (var row = 0; row < 4; row++) {
+		var tmp_row = this.matrix[row];
+		this.matrix[row] = this.matrix[7 - row];
+		this.matrix[7 - row] = tmp_row;
+	}
+
+	this.redraw();
+	this.bt_callback();
+}
+
 
 window.onload = function() {
 	log_box = document.getElementById("log");
 
-	recreate_matrix();
-
-	canvas = document.getElementById("draw");
-	canvas.addEventListener('click', function(event) {
-		var x;
-		var y;
-		if (event.x != undefined && event.y != undefined) {
-			x = event.x;
-			y = event.y;
-		}
-		else { // Firefox method to get the position
-			x = event.clientX
-				+ (document.body.scrollLeft || 0)
-				+ (document.documentElement.scrollLeft || 0);
-			y = event.clientY
-				+ (document.body.scrollTop || 0)
-				+ (document.documentElement.scrollTop || 0);
-		}
-		x -= canvas.offsetLeft || 0;
-		y -= canvas.offsetTop || 0;
-		var row = Math.floor(y / cell_dim_px);
-		var col = Math.floor(x / cell_dim_px);
-		if (rotated) {
-			var tmp = col;
-			col = row;
-			row = 7 - tmp;
-		}
-
-		matrix[row][col] = !matrix[row][col];
-		canvas_redraw();
-		calc_string_and_send();
-	}, false);
-
-	canvas_redraw();
-	calc_string_and_send();
-
-
 	if (!window.navigator.bluetooth)
 		log("Warning: The WebBluetooth API is not available in this browser!");
+
+	board = new Board(document.getElementById("draw"), calc_string_and_send);
+	calc_string_and_send();
 }
 
 function log(string) {
@@ -54,63 +145,36 @@ function log(string) {
 	log_box.scrollTop = log_box.scrollHeight;
 }
 
-function recreate_matrix() {
-	matrix = new Array(8);
-	for (var row = 0; row < 8; row++) {
-		matrix[row] = new Array(16);
-		for (var col = 0; col < 16; col++)
-			matrix[row][col] = false;
-	}
-}
-
+// Called from HTML
 function reset() {
-	recreate_matrix();
-	canvas_redraw();
-	calc_string_and_send();
+	board.reset();
 }
 
-function canvas_redraw() {
-	var ctx = canvas.getContext("2d");
-	ctx.shadowColor = '#f00';
-	ctx.fillStyle = "#000000";
-	ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-	var cell_offset = 3;
-	var cell_radius = (cell_dim_px - cell_offset * 2) / 2;
-	for (var row = 0; row < 8; row++) {
-		for (var col = 0; col < 16; col++) {
-			if (matrix[row][col]) {
-				ctx.fillStyle = "#ff0000";
-				ctx.shadowBlur = 20;
-			}
-			else {
-				ctx.fillStyle = "#ffefd5";
-				ctx.shadowBlur = 0;
-			}
-
-			ctx.beginPath();
-			ctx.arc(
-				cell_offset + col * cell_dim_px + cell_radius,
-				cell_offset + row * cell_dim_px + cell_radius,
-				cell_radius,
-				0, 2 * Math.PI);
-			ctx.fill();
-		}
-	}
+// Called from HTML
+function rotate() {
+	var canvas = document.getElementById('draw').classList.toggle('rotated');
+	var canvas_container = document.getElementById('draw-container').classList.toggle('rotated');
+	rotated = !rotated;
 }
+
+// Called from HTML
+function flip() {
+	board.flip();
+}
+
 
 function calc_string_and_send() {
 	var result_array = [];
 	for (var row = 0; row < 8; row++) {
 		var val = 0;
 		for (var col = 0; col < 8; col++)
-			val += (matrix[row][col] ? 1 : 0) * Math.pow(2, col);
+			val += (board.matrix[row][col] ? 1 : 0) * Math.pow(2, col);
 
 		result_array.push(val);
 
 		val = 0;
 		for (var col = 8; col < 16; col++)
-			val += (matrix[row][col] ? 1 : 0) * Math.pow(2, col - 8);
+			val += (board.matrix[row][col] ? 1 : 0) * Math.pow(2, col - 8);
 
 		result_array.push(val);
 	}
@@ -121,41 +185,6 @@ function calc_string_and_send() {
 	bt_send(result_array);
 }
 
-function rotate() {
-	var canvas = document.getElementById('draw').classList.toggle('rotated');
-	var canvas_container = document.getElementById('draw-container').classList.toggle('rotated');
-	rotated = !rotated;
-}
-
-function flip() {
-	for (var row = 0; row < 4; row++) {
-		var tmp_row = matrix[row];
-		matrix[row] = matrix[7 - row];
-		matrix[7 - row] = tmp_row;
-	}
-	canvas_redraw();
-	calc_string_and_send();
-}
-
-function fill_matrix(bt_arr) {
-	recreate_matrix();
-
-	var idx = 0;
-
-	for (var row = 0; row < 8; row++) {
-		var val = bt_arr[idx].charCodeAt(0);
-		for (var col = 0; col < 8; col++)
-			matrix[row][col] = ((val & 1 << col) > 0);
-
-		idx++;
-
-		val = bt_arr[idx].charCodeAt(0);
-		for (var col = 0; col < 8; col++)
-			matrix[row][col + 8] = ((val & 1 << col) > 0);
-
-		idx++;
-	}
-}
 
 function bt_connect() {
 	var device;
@@ -209,10 +238,9 @@ function bt_connect() {
 
 		var read_arr = bt_handle.readValue();
 		if (read_arr)
-			throw "Failed to read value"
+			throw "Failed to read value";
 
-		fill_matrix(read_arr);
-		canvas_redraw();
+		board.fill_mx(read_arr);
 	}
 	catch(err) {
 		log("Error: " + err);
